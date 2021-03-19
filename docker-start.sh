c38b23e8339438c841111ee2bf6c1914baf11b49
Yinn <43907800@qq.com>
2021-03-19 10:00:21 +0800

    删除启动脚本

--- docker-start.sh
+++
@@ -1,9 +0,0 @@
-git pull origin master
-
-mvn clean package
-
-docker stop zfile3
-docker rm zfile3
-docker rmi zfile3
-docker build -t zfile3 .
-docker run -p 8088:8088 --name zfile3 -e PROFILES_ACTIVE=prod-mysql -d zfile3
